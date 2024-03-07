e0f088b49451ca6e09f5033d477eee080ad1f7d1
BKWANGHO <bkwangho@naver.com>
2024-03-07 12:01:04 +0900

    240307 branch

--- Main.java
+++ Main.java
@@ -20,8 +20,8 @@ public class Main {
                     "0-종료  1-게시판  2-사용자관리 3-통장관리 4-Crawler");
             switch (sc.next()) {
                 case "0": return ;
-                case "1": BoardView.main(); break;
-                case "2": UserView.main(sc);  break;
+                case "1": UserView.main(sc);  break;
+                case "2": BoardView.main(); break;
                 case "3": AccountView.main(sc); break;
                 case "4": CrawlerView.main(sc); break;
             }
